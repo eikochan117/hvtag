6b9f443886cee06d059d8b3d4a1196a2ecf9bd27
eikochan117 <eiko.sub@outlook.com>
2024-10-06 14:29:31 +0200

    change from hvdb to dlsite for tags fetching + add folder download

--- hvdb-tagger/__main__.py
+++ hvdb-tagger/__main__.py
@@ -9,6 +9,13 @@ import requests
 import argparse
 import shutil
 from bs4 import BeautifulSoup
+import yaml
+
+import asyncio
+from dlsite_async import DlsiteAPI
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 def convertWide(t):
     t = t.replace("０", "0")
     t = t.replace("１", "1")
@@ -23,105 +30,115 @@ def convertWide(t):
     t = t.replace("：", ":")
     return t
 
-blacklistedTags = ["non-mega link", "broken link", "mp3 only", "outdated"]
-jpregex = r"[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]"
+uncensoredTags = yaml.safe_load(open(os.getenv("HVDB_PATH") + "/uncensoredTags.yaml", "r"))
 
+def uncensorTag(t):
+    if t in uncensoredTags : 
+        return uncensoredTags[t]
+    return t
+
+async def query_work(rj):
+    async with DlsiteAPI(locale="en_US") as api:
+        return await api.get_work(rj)
+
+async def query_circle(rg):
+    async with DlsiteAPI() as api:
+        return await api.get_circle(rg)
 
 def tag(cw, args):
     rjcode = os.path.basename(cw)
-    print("collecting HVDB data... ")
-    url = "https://hvdb.me/Dashboard/WorkDetails/" + rjcode
-    txt = requests.get(url)
-    soup = BeautifulSoup(txt.text, "html.parser")
-
-    album = soup.find("label", id="circleLabel").get_text().strip()
-    print("Album : " + album)
-    circle = soup.find("a", href=re.compile("CircleWorks")).get_text().split("/")[0].strip()
-    print("Circle : " + circle)
-    print("tags:")
-    tags_q = soup.find_all("a", href=re.compile("TagWorks"))
-    tags = list()
-    for t in tags_q:
-        tt = t.get_text()
-        if not tt in blacklistedTags:
-            tags.append(tt)
-    print(tags)
-    print("CVs:")
-    cvs_q = soup.find_all("a", href=re.compile("CVWorks"))
+    work = asyncio.run(query_work(rjcode))
+    print("collecting DLsite data...")
+    album = work.work_name
+    print("collecting real name of circle "+ work.circle + "...")
+    circleDl = asyncio.run(query_circle(work.maker_id))
+    circle = circleDl.maker_name
     cvs = list()
-    for cv in cvs_q:
-        name = cv.get_text()
-        if "N/A" in name:
-            cvs.append("Missing CV")
-        else:
-            if not args["jp"] or re.match(jpregex, name):
-                cvs.append(name)
-    print(cvs)
-
+    if work.voice_actor != None:
+        for cv in work.voice_actor:
+            cvs.append(cv)
+    tags = list()
+    if work.genre != None:
+        for tag in work.genre:
+            tags.append(uncensorTag(tag.lower()))
     files = [f for f in os.listdir(cw) if f.endswith(".mp3")]
 
     for f in files:
         fname = f.replace(".mp3", "")
-        useNoname = args["first"]
-        useFirstChar = False
-        splitChar = args["split"]
+        if not args["tags"]:
+            useNoname = args["first"]
+            useFirstChar = False
+            splitChar = args["split"]
 
-        if len(args["remove"]) > 0 :
-            fname = fname.replace(args["remove"], "")
-        if args["space"]:
-            splitChar = " "
-        if args["wide"]:
-            fname = convertWide(fname)
+            if len(args["remove"]) > 0 :
+                fname = fname.replace(args["remove"], "")
+            if args["space"]:
+                splitChar = " "
+            if args["wide"]:
+                fname = convertWide(fname)
 
-        num = "0"
-        trackName = fname
-        if useNoname:
-            if len(files) > 1:
-                num = re.sub(r'\D', '', fname)
-        elif useFirstChar:
-            i = 0
-            while f[i].isdigit():
-                i = i + 1
-            num = f[0:i]
-        else:
-            splits = fname.split(splitChar)
-            trackNameIndex = min(args["index"] + 1, len(splits) - 1)
-            trackName = splits[trackNameIndex]
-            num = re.sub(r'\D', '', splits[args["index"]])
-
-        print(f)
-        print("   Tr." + num + " : " + trackName)
+            num = "0"
+            trackName = fname
+            if useNoname:
+                if len(files) > 1:
+                    num = re.sub(r'\D', '', fname)
+            elif useFirstChar:
+                i = 0
+                while f[i].isdigit():
+                    i = i + 1
+                num = f[0:i]
+            else:
+                splits = fname.split(splitChar)
+                trackNameIndex = min(args["index"] + 1, len(splits) - 1)
+                trackName = splits[trackNameIndex]
+                num = re.sub(r'\D', '', splits[args["index"]])
+            print(f)
+            print("   Tr." + num + " : " + trackName)
         filePath = cw + "/" + f
         try : 
             m = EasyID3(filePath)
         except mutagen.id3.ID3NoHeaderError:
             m = mutagen.File(filePath, easy=True)
             m.add_tags()
-        m["tracknumber"] = num
-        m["title"] = trackName
+        if not args["tags"]:
+            m["tracknumber"] = num
+            m["title"] = trackName
         m["album"] = album
-        m["artist"] = "\x00".join(cvs)
-        m["genre"] = "\x00".join(tags)
+        if len(cvs) == 0 :
+            m["artist"] = "Missing CVs"
+        else :
+            m["artist"] = "\x00".join(cvs)
+        if len(tags) == 0:
+            m["genre"] = "Missing tags"
+        else:
+            m["genre"] = "\x00".join(tags)
         m["albumartist"] = circle
         m["titlesort"] = rjcode
         m.save()
+    if args["image"]:
+        print("Downloading work image...")
+        url = work.work_image.replace("//", "https://")
+        img = requests.get(url, stream=True)
+        with open(cw + "/folder.jpeg", "wb") as f:
+            shutil.copyfileobj(img.raw, f)
+        print("Done.")
     open(cw + "/.tagged", 'a').close()
     if args["move"] != "":
-        print("Moving " + cw + " to " + newPath + " ...")
+        print("Moving " + cw + " to " + args["move"] + " ...")
         newPath = shutil.move(cw, args["move"])
         print("Done.")
-
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
-        description="Tag ASMR works using tags from HVDB."
+        description="Tag ASMR works using tags from Dlsite."
     )
 
     parser.add_argument("--batch", "-b", action="store_true", help="Process multiple works delimited by folder, named according to their RJ code.")
     parser.add_argument("--force", "-f", action="store_true", help="Force tagging even with .tagged file present.")
     parser.add_argument("--clean", "-c", action="store_true", help="Remove .tagged files.")
-    parser.add_argument("--jp", "-j", action="store_true", help="Only keep JP names.")
+    parser.add_argument("--tags", "-t", action="store_true", help="Keep track number, only update tags.", default=False)
+    parser.add_argument("--image", action="store_true", help="Add/Replace folder.jpeg with Dlsite's work image.", default=False)
     parser.add_argument("--move", "-m", type=str, help="Move tagged folder to destination.", default="")
 
     singleParser = argparse.ArgumentParser()
@@ -130,7 +147,6 @@ if __name__ == "__main__":
     singleParser.add_argument("--space", "-s", action="store_true", help="Use space as spliter")
     singleParser.add_argument("--wide", "-w", action="store_true", help="Convert wide number")
     singleParser.add_argument("--index", "-i", type=int, help="Specify split index", default=0)
-    singleParser.add_argument("--jp", "-j", action="store_true", help="Only keep JP names.")
     singleParser.add_argument("--move", "-m", type=str, help="Move tagged folder to destination.", default="")
     singleParser.add_argument("split", default="\x00")
 
@@ -148,9 +164,12 @@ if __name__ == "__main__":
                     files = [f for f in os.listdir(cwd + "/" + folder) if f.endswith(".mp3")]
                     if len(files) > 0 :
                         print(files[0])
-                        args = vars(singleParser.parse_args(input().split(" ")))
-                        if kwargs["jp"]:
-                            args["jp"] = True
+                        if not kwargs["tags"] :
+                            args = vars(singleParser.parse_args(input().split(" ")))
+                        else :
+                            args = kwargs
+                        args["tags"] = kwargs["tags"]
+                        args["image"] = kwargs["image"]
                         if kwargs["move"] != "":
                             args["move"] = kwargs["move"]
                         tag(folder, args)
