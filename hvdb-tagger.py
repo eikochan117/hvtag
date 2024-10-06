912f812b3c2ee02a7f13880b69b1b5256643493b
eikochan117 <eiko.sub@outlook.com>
2024-10-06 14:29:28 +0200

    fix #2, #3, #5 and #9

--- hvdb-tagger.py
+++ hvdb-tagger.py
@@ -7,60 +7,110 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-cwd = os.getcwd()
-rjcode = os.path.basename(cwd)
-splitChar = sys.argv[1]
-
 bl_tags = ["non-mega link", "broken link", "mp3 only", "outdated"]
 
+def tag(cw, command):
+    rjcode = os.path.basename(cw)
+    print("collecting HVDB data... ")
+    url = "https://hvdb.me/Dashboard/WorkDetails/" + rjcode
+    txt = requests.get(url)
+    soup = BeautifulSoup(txt.text, "html.parser")
 
-print("collecting HVDB data... ")
-url = "https://hvdb.me/Dashboard/WorkDetails/" + rjcode
-txt = requests.get(url)
-soup = BeautifulSoup(txt.text, "html.parser")
+    album = soup.find("label", id="circleLabel").get_text().strip()
+    print("Album : " + album)
+    circle = soup.find("a", href=re.compile("CircleWorks")).get_text().split("/")[0].strip()
+    print("Circle : " + circle)
+    print("tags:")
+    tags_q = soup.find_all("a", href=re.compile("TagWorks"))
+    tags = list()
+    for t in tags_q:
+        tt = t.get_text()
+        if not tt in bl_tags:
+            tags.append(tt)
+    print(tags)
+    print("CVs:")
+    cvs_q = soup.find_all("a", href=re.compile("CVWorks"))
+    cvs = list()
+    for cv in cvs_q:
+        if "N/A" in cv.get_text():
+            cvs.append("Missing CV")
+        else:
+            cvs.append(cv.get_text())
+    print(cvs)
 
-album = soup.find("label", id="circleLabel").get_text().strip()
-print("Album : " + album)
-circle = soup.find("a", href=re.compile("CircleWorks")).get_text().split("/")[0].strip()
-print("Circle : " + circle)
-print("tags:")
-tags_q = soup.find_all("a", href=re.compile("TagWorks"))
-tags = list()
-for t in tags_q:
-    tt = t.get_text()
-    if not tt in bl_tags:
-        tags.append(tt)
-print(tags)
-print("CVs:")
-cvs_q = soup.find_all("a", href=re.compile("CVWorks"))
-cvs = list()
-for cv in cvs_q:
-    if "N/A" in cv.get_text():
-        cvs.append("Missing CV")
-    else:
-        cvs.append(cv.get_text())
-print(cvs)
+    # urlimg = "https://hvdb.me/WorkImages/RJ" + rjcode + ".jpg"
+    # img = requests.get(urlimg).content
+    # with open("folder.jpg", "wb") as f:
+    #     f.write(img)
 
-# urlimg = "https://hvdb.me/WorkImages/RJ" + rjcode + ".jpg"
-# img = requests.get(urlimg).content
-# with open("folder.jpg", "wb") as f:
-#     f.write(img)
+    files = [f for f in os.listdir(cw) if f.endswith(".mp3")]
 
-files = [f for f in os.listdir(cwd) if f.endswith(".mp3")]
+    for f in files:
+        fname = f.replace(".mp3", "")
+        cs = command.split(" ")
+        useNoname = False
+        useFirstChar = False
+        splitChar = command
+        i = 0
+        while i < len(cs):
+            c = cs[i]
+            if "--remove" in c:
+                i += 1
+                ss = cs[i]
+                fname = fname.replace(ss, "")
+            elif "--no-title" in c:
+                useNoname = True
+            elif "--first-char" in c:
+                useFirstChar = True
+            elif i == len(cs) - 1:
+                splitChar = c
+            i += 1
 
-for f in files:
-    fname = f.replace(".mp3", "")
-    splits = fname.split(splitChar)
-    num = re.sub(r'\D', '', splits[0])
-    trackName = splits[1]
-    print(f)
-    print("   Tr." + num + " : " + trackName)
-    m = EasyID3(f)
-    m["tracknumber"] = num
-    m["title"] = trackName
-    m["album"] = album
-    m["artist"] = "/".join(cvs)
-    m["genre"] = "/".join(tags)
-    m["albumartist"] = circle
-    m["titlesort"] = rjcode
-    m.save()
+        num = "1"
+        trackName = fname
+        if useNoname:
+            if len(files) > 1:
+                num = re.sub(r'\D', '', fname)
+        elif useFirstChar:
+            num = trackName[0]
+        else:
+            splits = fname.split(splitChar)
+            trackName = splits[1]
+            num = re.sub(r'\D', '', fname)
+
+        print(f)
+        print("   Tr." + num + " : " + trackName)
+        m = EasyID3(cw + "/" + f)
+        m["tracknumber"] = num
+        m["title"] = trackName
+        m["album"] = album
+        m["artist"] = "/".join(cvs)
+        m["genre"] = "/".join(tags)
+        m["albumartist"] = circle
+        m["titlesort"] = rjcode
+        m.save()
+
+
+mode = "single"
+if  "--batch" in sys.argv:
+    mode = "batch"
+
+cwd = os.getcwd()
+splitChar = sys.argv[1]
+
+if mode == "batch":
+    folders = [f for f in os.listdir(cwd) if os.path.isdir(os.path.join(cwd, f))]
+    for folder in folders:
+        if "RJ" in folder:
+            print(folder)
+            files = [f for f in os.listdir(cwd + "/" + folder) if f.endswith(".mp3")]
+            if len(files) > 0 :
+                print(files[0])
+                command = input("Command ([--remove <text>, --no-title, --first-char, --wide] <separator>) : ")
+                tag(folder, command)
+            else:
+                print("No valid file found in " + folder + " !")
+else:
+    tag(cwd, splitChar)
+
+
